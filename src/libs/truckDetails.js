4965000b6e9fcdafb0416086841c8243bed22af1
rashminder2002 <80933649+rashminder2002@users.noreply.github.com>
2024-01-25 21:42:57 -0500

    Updated Truck routes

--- src/libs/truckDetails.js
+++ src/libs/truckDetails.js
@@ -1,7 +1,7 @@
 const TruckDetail = require('../models/TruckDetail');
 
-async function addTruck (truck) {
-    let newTruck = new TruckDetail ({
+async function addTruck(truck) {
+    let newTruck = new TruckDetail({
         truckNumber: truck.truckNumber,
         make: truck.make,
         model: truck.model,
@@ -16,8 +16,7 @@ async function addTruck (truck) {
     return newTruck;
 }
 
-
-async function getAllTrucks () {
+async function getAllTrucks() {
     let trucks;
     try {
         trucks = await TruckDetail.find();
@@ -27,5 +26,38 @@ async function getAllTrucks () {
     return trucks;
 }
 
+async function getTruckById(id) {
+    let truck;
+    try {
+        truck = await TruckDetail.findById(id);
+    } catch (err) {
+        console.error(err);
+    }
+    return truck;
+}
+
+async function updateTruckById(id, updatedTruck) {
+    let truck;
+    try {
+        updatedTruck.updatedAt = Date.now();
+        truck = await TruckDetail.findByIdAndUpdate(id, updatedTruck, { new: true });
+    } catch (err) {
+        console.error(err);
+    }
+    return truck;
+}
+
+async function deleteTruckById(id) {
+    try {
+        const truck = await TruckDetail.findByIdAndRemove(id);
+        return truck;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 module.exports.addTruck = addTruck;
 module.exports.getAllTrucks = getAllTrucks;
+module.exports.getTruckById = getTruckById;
+module.exports.updateTruckById = updateTruckById;
+module.exports.deleteTruckById = deleteTruckById;
--- src/routes/truckDetails.js
+++ src/routes/truckDetails.js
@@ -2,24 +2,62 @@ const express = require("express");
 const router = express.Router();
 const truckDetailsLib = require("../libs/truckDetails");
 
+// Fetch All Trucks
+router.get("/", async (req, res) => {
+  try {
+    const trucks = await truckDetailsLib.getAllTrucks();
+    res.json(trucks);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Fetch One Truck
+router.get("/:id", async (req, res) => {
+  try {
+    const truck = await truckDetailsLib.getTruckById(req.params.id);
+    if (truck == null) {
+      res.status(404).json({ message: "Cannot find truck with the requested id" });
+    } else {
+      res.json(truck);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 // Create Truck
 router.post("/", async (req, res) => {
   try {
     const newTruck = await truckDetailsLib.addTruck(req.body);
     res.json(newTruck);
   } catch (err) {
-    console.error("Error creating Truck:", err);
+    console.error("Error creating truck:", err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
-// Fetch All Trucks
-router.get("/", async (req, res) => {
+// Update Truck
+router.patch("/:id", async (req, res) => {
   try {
-    const truck = await truckDetailsLib.getAllTrucks();
-    res.json(truck);
+    const updatedTruck = await truckDetailsLib.updateTruckById(req.params.id, req.body);
+    res.json(updatedTruck);
   } catch (err) {
     console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Delete Truck
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedTruck = await truckDetailsLib.deleteTruckById(req.params.id);
+    res.json(deletedTruck);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
