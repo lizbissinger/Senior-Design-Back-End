c75c3548f62ebefd2b9284e8eefe055943e3c5c4
rashminder2002 <80933649+rashminder2002@users.noreply.github.com>
2024-01-25 21:42:38 -0500

    Updated Driver routes

--- src/libs/driverDetails.js
+++ src/libs/driverDetails.js
@@ -1,7 +1,7 @@
 const DriverDetail = require('../models/DriverDetail');
 
-async function addDriver (driver) {
-    let newDriver = new DriverDetail ({
+async function addDriver(driver) {
+    let newDriver = new DriverDetail({
         name: driver.name,
         licenseNumber: driver.licenseNumber,
         phoneNumber: driver.phoneNumber,
@@ -15,8 +15,7 @@ async function addDriver (driver) {
     return newDriver;
 }
 
-
-async function getAllDrivers () {
+async function getAllDrivers() {
     let drivers;
     try {
         drivers = await DriverDetail.find();
@@ -26,5 +25,38 @@ async function getAllDrivers () {
     return drivers;
 }
 
+async function getDriverById(id) {
+    let driver;
+    try {
+        driver = await DriverDetail.findById(id);
+    } catch (err) {
+        console.error(err);
+    }
+    return driver;
+}
+
+async function updateDriverById(id, updatedDriver) {
+    let driver;
+    try {
+        updatedDriver.updatedAt = Date.now();
+        driver = await DriverDetail.findByIdAndUpdate(id, updatedDriver, { new: true });
+    } catch (err) {
+        console.error(err);
+    }
+    return driver;
+}
+
+async function deleteDriverById(id) {
+    try {
+        const driver = await DriverDetail.findByIdAndRemove(id);
+        return driver;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 module.exports.addDriver = addDriver;
 module.exports.getAllDrivers = getAllDrivers;
+module.exports.getDriverById = getDriverById;
+module.exports.updateDriverById = updateDriverById;
+module.exports.deleteDriverById = deleteDriverById;
--- src/routes/driverDetails.js
+++ src/routes/driverDetails.js
@@ -2,7 +2,33 @@ const express = require('express');
 const router = express.Router();
 const driverDetailsLib = require('../libs/driverDetails');
 
-// Create driver
+// Fetch All Drivers
+router.get('/', async (req, res) => {
+    try {
+        const drivers = await driverDetailsLib.getAllDrivers();
+        res.json(drivers);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Fetch One Driver
+router.get('/:id', async (req, res) => {
+    try {
+        const driver = await driverDetailsLib.getDriverById(req.params.id);
+        if (driver == null) {
+            res.status(404).json({ message: 'Cannot find driver with the requested id' });
+        } else {
+            res.json(driver);
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Create Driver
 router.post('/', async (req, res) => {
     try {
         const newDriver = await driverDetailsLib.addDriver(req.body);
@@ -13,16 +39,26 @@ router.post('/', async (req, res) => {
     }
 });
 
-
-// Fetch All Drivers
-router.get('/', async (req, res) => {
+// Update Driver
+router.patch('/:id', async (req, res) => {
     try {
-        const driver = await driverDetailsLib.getAllDrivers();
-        res.json(driver);
+        const updatedDriver = await driverDetailsLib.updateDriverById(req.params.id, req.body);
+        res.json(updatedDriver);
     } catch (err) {
         console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
+// Delete Driver
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedDriver = await driverDetailsLib.deleteDriverById(req.params.id);
+        res.json(deletedDriver);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 module.exports = router;
