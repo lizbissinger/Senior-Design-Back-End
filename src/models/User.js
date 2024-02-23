9fa7edc7147092e2244ddf3c8214e3a860759d2f
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2024-02-23 14:13:03 -0500

    model for User info

---
+++ src/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+//add further attributes here is needed ei. phone, email, etc.
+const userSchema = new mongoose.Schema({
+    clerkUserId: {type: String, required: true, unique:true},
+    firstName: {type:String},
+    lastName: {type: String},
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
